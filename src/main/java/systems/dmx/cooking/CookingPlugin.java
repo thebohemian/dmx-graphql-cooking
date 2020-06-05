fa865bce1f166e78c04a64242fe406a6ed297bda
Silke Meyer <silke.meyer@dmx.berlin>
2020-06-05 20:26:21 +0200

    Adapted cooking plugin for 5.0-SNAPSHOT, assocAutoTyping

--- src/main/java/systems/dmx/cooking/CookingPlugin.java
+++ src/main/java/systems/dmx/cooking/CookingPlugin.java
@@ -16,59 +16,59 @@ public class CookingPlugin extends PluginActivator implements PreCreateAssoc {
     public void preCreateAssoc(AssocModel assoc) {
         // Associations from instances of the topic types listed below to instances of "Dish" shall always be "Ingredient amount" associations.
         // Baking ingredient <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.cooking.baking_ingredient", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.cooking.baking_ingredient", "dmx.cooking.dish",
             "dmx.cooking.ingredient_amount", "dmx.core.default", "dmx.core.default");
         // Cereal product <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.cooking.cereal_product", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.cooking.cereal_product", "dmx.cooking.dish",
             "dmx.cooking.ingredient_amount", "dmx.core.default", "dmx.core.default");
         // Dairy product <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.cooking.dairy_product", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.cooking.dairy_product", "dmx.cooking.dish",
             "dmx.cooking.ingredient_amount", "dmx.core.default", "dmx.core.default");
         // Fish <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.cooking.fish", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.cooking.fish", "dmx.cooking.dish",
             "dmx.cooking.ingredient_amount", "dmx.core.default", "dmx.core.default");
         // Fruit product <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.cooking.fruit", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.cooking.fruit", "dmx.cooking.dish",
             "dmx.cooking.ingredient_amount", "dmx.core.default", "dmx.core.default");
         // Herb <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.cooking.herb", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.cooking.herb", "dmx.cooking.dish",
             "dmx.cooking.ingredient_amount", "dmx.core.default", "dmx.core.default");
         // Legume <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.cooking.legume", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.cooking.legume", "dmx.cooking.dish",
             "dmx.cooking.ingredient_amount", "dmx.core.default", "dmx.core.default");
         // Liquid <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.cooking.liquid", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.cooking.liquid", "dmx.cooking.dish",
             "dmx.cooking.ingredient_amount", "dmx.core.default", "dmx.core.default");
         // Meat <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.cooking.meat", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.cooking.meat", "dmx.cooking.dish",
             "dmx.cooking.ingredient_amount", "dmx.core.default", "dmx.core.default");
         // Nut <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.cooking.nut", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.cooking.nut", "dmx.cooking.dish",
             "dmx.cooking.ingredient_amount", "dmx.core.default", "dmx.core.default");
         // Oil <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.cooking.oil", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.cooking.oil", "dmx.cooking.dish",
             "dmx.cooking.ingredient_amount", "dmx.core.default", "dmx.core.default");
         // Poultry <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.cooking.poultry", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.cooking.poultry", "dmx.cooking.dish",
             "dmx.cooking.ingredient_amount", "dmx.core.default", "dmx.core.default");
         // Sauce <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.cooking.sauce", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.cooking.sauce", "dmx.cooking.dish",
             "dmx.cooking.ingredient_amount", "dmx.core.default", "dmx.core.default");
         // Soy product <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.cooking.soy_product", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.cooking.soy_product", "dmx.cooking.dish",
             "dmx.cooking.ingredient_amount", "dmx.core.default", "dmx.core.default");
         // Spice <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.cooking.spice", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.cooking.spice", "dmx.cooking.dish",
             "dmx.cooking.ingredient_amount", "dmx.core.default", "dmx.core.default");
         // Sweetener <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.cooking.sweetener", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.cooking.sweetener", "dmx.cooking.dish",
             "dmx.cooking.ingredient_amount", "dmx.core.default", "dmx.core.default");
         // Vegetable <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.cooking.vegetable", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.cooking.vegetable", "dmx.cooking.dish",
             "dmx.cooking.ingredient_amount", "dmx.core.default", "dmx.core.default");
         // Associations from instances of "Bookmark" to instances of "Dish" shall always be Custom Association Types "Source".
         // Bookmark <-> Dish
-        DMXUtils.associationAutoTyping(assoc, "dmx.bookmarks.bookmark", "dmx.cooking.dish",
+        DMXUtils.assocAutoTyping(assoc, "dmx.bookmarks.bookmark", "dmx.cooking.dish",
             "dmx.cooking.source", "dmx.core.default", "dmx.core.default");
     }
 }
